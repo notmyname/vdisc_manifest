8aa54f4f6b66d11dc777d524a71b8d771aeac8e8
John Dickinson <me@not.mn>
2019-11-05 16:21:30 -0800

    Initial import
    This is a tool to create a vdisc csv manifest file from a given set
    of Swift containers. It uses the python-swiftclient library to auth,
    query the cluster, and generate the appropriate listings.
    
    Usage is ./create_manifest.py c1 [c2 [c3 [...]]]
    
    Redirect the output to a csv file to use it with vdisk.
    
    The current vdisc swift url is:
      swift://<endpoint>/<container_owner>/<container>/path/to/some.obj
    
    Signed-off-by: John Dickinson <me@not.mn>

---
+++ create_manifest.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3.7
+
+"""
+given a container name, build a vdisc csv manifest for every object
+ * use the swiftclient sdk service module for this
+
+eventually, allow passing in same parameters as swiftclient for listing filters
+"""
+
+import sys
+from urllib.parse import quote
+import string
+
+from swiftclient import service
+
+vdisc_csv_template = '"%s","%s",%d'
+
+good_chars = string.ascii_letters + string.digits + "/" + "-" + "_" + "."
+
+svc = service.SwiftService()  # will pull auth creds from env vars
+c = service.get_conn(svc._options)
+storage_url = c.get_auth()[0].replace("https", "swift").replace("http", "swift")
+storage_url = storage_url[:storage_url.index("/v1")]
+for container in sys.argv[1:]:
+    l = svc.list(container=container)
+    for result in l:
+        for item in result["listing"]:
+            n = ""
+            for char in item["name"]:
+                if char in good_chars:
+                    n += char
+                else:
+                    n += "-"
+            local_name = "/%s/%s" % (container, n)
+            local_name = local_name.replace(" ", "_")
+            url = "%s/%s/%s/%s" % (storage_url, c.user, container, quote(item["name"]))
+            print(vdisc_csv_template % (local_name, url, item["bytes"]))
