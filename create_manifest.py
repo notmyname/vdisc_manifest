9d4019cba98ca72ec36c21c002d5bfe670154ad9
John Dickinson <me@not.mn>
2019-11-06 12:52:13 -0800

    use regex to clean the path name instead of character by character walk

--- create_manifest.py
+++ create_manifest.py
@@ -10,6 +10,7 @@ eventually, allow passing in same parameters as swiftclient for listing filters
 import sys
 from urllib.parse import quote
 import string
+import re
 
 from swiftclient import service
 
@@ -20,18 +21,13 @@ good_chars = string.ascii_letters + string.digits + "/" + "-" + "_" + "."
 svc = service.SwiftService()  # will pull auth creds from env vars
 c = service.get_conn(svc._options)
 storage_url = c.get_auth()[0].replace("https", "swift").replace("http", "swift")
-storage_url = storage_url[:storage_url.index("/v1")]
+storage_url = storage_url[: storage_url.index("/v1")]
 for container in sys.argv[1:]:
     l = svc.list(container=container)
     for result in l:
         for item in result["listing"]:
-            n = ""
-            for char in item["name"]:
-                if char in good_chars:
-                    n += char
-                else:
-                    n += "-"
-            local_name = "/%s/%s" % (container, n)
+            clean_name = re.sub("[^a-zA-Z0-9/_.-]", "-", item["name"])
+            local_name = "/%s/%s" % (container, clean_name)
             local_name = local_name.replace(" ", "_")
             url = "%s/%s/%s/%s" % (storage_url, c.user, container, quote(item["name"]))
             print(vdisc_csv_template % (local_name, url, item["bytes"]))
